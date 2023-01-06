fbc7c80e91f069d7f4046cb8d6dbbdb1ebd1753e
aCoder1123 <bailey.tuckman@westtown.edu>
2023-01-06 07:24:43 -0500

    added pinouts doc

---
+++ Python/pinout.py
@@ -0,0 +1,26 @@
+zPins = {
+    "base": 27,
+    "lower": 29,
+    "mid": 31,
+    "high": 33,
+    "rotary": 35,
+    "actuator": 37,
+}
+
+sPins = {
+    "base": 11,
+    "lower": 13,
+    "mid": 15,
+    "high": 19,
+    "rotary": 21,
+    "actuator": 23,
+}
+
+dPins = {
+    "base": 16,
+    "lower": 18,
+    "mid": 22,
+    "high": 24,
+    "rotary": 26,
+    "actuator": 28,
+}

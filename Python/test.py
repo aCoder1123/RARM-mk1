651dc299e2f0a33a72bbf387176a99067d08faab
aCoder1123 <bailey.tuckman@westtown.edu>
2023-01-06 22:00:12 -0500

    more testing

--- Python/test.py
+++ Python/test.py
@@ -2,9 +2,15 @@ from constants import *
 GPIO.setmode(GPIO.BOARD)
 
 
-test = Joint([3, 5, 100])
-
+test = Joint([3, 5, 7])
+print(1)
+wait(100)
+print(2)
 print('starting')
 
+test.angle = 100
 test.moveJoint(0, 1, 100)
 test.moveJoint(1, 1, 100)
+
+print("stopping")
+GPIO.cleanup()

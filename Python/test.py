e24fdfd240770402b2fccd66bd4845d3f9218806
aCoder1123 <bailey.tuckman@westtwown.edu>
2023-03-16 14:22:13 -0400

    more testing

--- Python/test.py
+++ Python/test.py
@@ -2,18 +2,22 @@ from constants import *
 from time import sleep
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.OUT) 
-GPIO.setup(5, GPIO.OUT)
-GPIO.output(5, GPIO.HIGH)
+GPIO.setup(75, GPIO.OUT) 
+# GPIO.setup(5, GPIO.OUT)
+# GPIO.output(5, GPIO.LOW)
+# GPIO.setup(7, GPIO.OUT)
+# GPIO.output(7, GPIO.LOW)
 x =1
-for i in range(0, 10000): 
+for i in range(0, 300): 
             print(x)
             x+=1
-            GPIO.output(3, (GPIO.HIGH))
-            sleep(0.04)
-            GPIO.output(3, (GPIO.LOW))
-            sleep(0.04)
+            GPIO.output(7, (GPIO.HIGH))
+            sleep(0.02)
+            GPIO.output(7, (GPIO.LOW))
+            sleep(0.02)
+            
 print("stopping")
+# GPIO.output(7, GPIO.LOW)
 GPIO.cleanup()
 
 # print(time.time())

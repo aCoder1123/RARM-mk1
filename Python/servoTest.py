5e0901c74352906b92f141138983192b04f4130b
aCoder1123 <bailey.tuckman@westtwown.edu>
2023-03-20 10:55:32 -0400

    servo testing

--- Python/servoTest.py
+++ Python/servoTest.py
@@ -6,11 +6,22 @@ servo = AngularServo(17, min_angle=-90, max_angle=90)
 while True:
     servo.angle = -90
     sleep(2)
-    servo.angle = -45
+    servo.min()
     sleep(2)
     servo.angle = 0
     sleep(2)
-    servo.angle = 45
-    sleep(2)
     servo.angle = 90
     sleep(2)
+    
+
+# from gpiozero import Servo
+# from time import sleep
+# servo = Servo(17)
+
+# while True:
+#     servo.value = 2
+#     sleep(2)
+#     servo.value = 0
+#     sleep(2)
+#     # servo.value(-1)
+#     # sleep(2)

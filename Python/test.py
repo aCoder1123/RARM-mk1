1a0348393cbf873aea024ba8bf99bb921d704c5e
aCoder1123 <bailey.tuckman@westtown.edu>
2023-03-20 11:00:13 -0400

    cleaned up file

--- Python/test.py
+++ Python/test.py
@@ -9,15 +9,13 @@ GPIO.setup(75, GPIO.OUT)
 # GPIO.output(7, GPIO.LOW)
 x =1
 for i in range(0, 300): 
-            print(x)
-            x+=1
-            GPIO.output(7, (GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(7, (GPIO.LOW))
-            sleep(0.02)
+    print(x)
+    x+=1
+    GPIO.output(7, (GPIO.HIGH))
+    sleep(0.02)
+    GPIO.output(7, (GPIO.LOW))
+    sleep(0.02)
             
 print("stopping")
 # GPIO.output(7, GPIO.LOW)
 GPIO.cleanup()
-
-# print(time.time())

b3f3f6d01c900c54d3c37a88d0786bbcfc788901
aCoder1123 <bailey.tuckman@westtown.edu>
2023-03-20 11:00:47 -0400

    cleaned up for IKE restructuring

--- Python/functions.py
+++ Python/functions.py
@@ -1,8 +1,5 @@
 from constants import *
-from pinout import *
-
-# GPIO = webiopi.GPIO
-
+from pinout import getPins
 
 
 ROTATATIONAL_ANGLE = 0
@@ -12,23 +9,3 @@ TOP_ARM_ANGLE = 0
 HAND_ANGLE = 0
 HAND_OPENESS = 1
 HAND_POSITION = [0, 0, 0]
-
-
-
-def point():
-    pass
-
-def getPosition() -> list:
-    pass
-
-def moveDirection(direction: list):
-    pass
-
-
-# c= sqrt a2+b2﹣2abcosγ
-
-#cos^-1((c2-a2-b2)/-2ab) = theta
-
-def getAngles(x: int, y: int, z: int) -> list:
-    pass
-
--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,9 @@
 from functions import *
 
+
 SmoothingConstant = 1
 
 GPIO.setmode(GPIO.BOARD)
-
-
-baseJoint = Joint([sPins['base'], dPins['base'], zPins['base']], None)
-lowerJoint = Joint([sPins['lower'], dPins['lower'], zPins['lower']])
-midJoint = Joint([sPins['mid'], dPins['mid'], zPins['mid']])
-highJoint = Joint([sPins['high'], dPins['high'], zPins['high']])
-rotaryJoint = Joint([sPins['rotary'], dPins['rotary'], zPins['rotary']], None)
-actuatorJoint = Joint([sPins['actuator'], dPins['actuator'], zPins['actuator']])
 
 
 @webiopi.macro

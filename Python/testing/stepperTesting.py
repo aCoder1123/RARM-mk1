32000bd3c2e6897e049fabf3d6c4c4adccc5d996
aCoder1123 <bailey.tuckman@westtwown.edu>
2023-11-26 16:30:41 -0500

    stepper testing

--- Python/testing/stepperTesting.py
+++ Python/testing/stepperTesting.py
@@ -1,24 +1,25 @@
 import RPi.GPIO as GPIO
 from miscTest import wait as wait
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-stepPin = 3
-dirPin = 5
-speed = 0.0004
+stepPin = 15
+dirPin = 22
+speed = 0.01
 
 GPIO.setup(stepPin, GPIO.OUT)
 GPIO.setup(dirPin, GPIO.OUT)
 
 print(1)
 GPIO.output(dirPin, GPIO.LOW)
-for i in range(16000):
+for i in range(400):
     GPIO.output(stepPin, GPIO.HIGH)
+    wait(speed)
     GPIO.output(stepPin, GPIO.LOW)
     wait(speed)
 
 print(2)
 GPIO.output(dirPin, GPIO.HIGH)
-for i in range(1600):
+for i in range(400):
     GPIO.output(stepPin, GPIO.HIGH)
     GPIO.output(stepPin, GPIO.LOW)
     wait(speed)
@@ -26,4 +27,4 @@ for i in range(1600):
 print("DONE")
 
     
-
+GPIO.cleanup()

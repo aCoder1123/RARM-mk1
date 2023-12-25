59958b6afd1b2a6bf6a24e94c98b636e82fc083a
aCoder1123 <bailey.tuckman@westtwown.edu>
2023-12-25 16:56:54 -0500

    tested multithreading

--- Python/testing/multiThreadingTest.py
+++ Python/testing/multiThreadingTest.py
@@ -2,8 +2,50 @@ from threading import Thread
 from time import sleep
 import RPi.GPIO as GPIO
 
-def blink(pin):
-    for i in range(100):
-        
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
 
-GPIO.setmode(GPIO.BCM)
+
+first_pin = 11
+second_pin = 13
+third_pin = 15
+
+def blink(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    print("blinking")
+    for i in range(20):
+        GPIO.output(pin, GPIO.HIGH)
+        sleep(.1)
+        GPIO.output(pin, GPIO.LOW)
+        sleep(.3)
+
+
+def blink2(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    print("blinking")
+    for i in range(50):
+        GPIO.output(pin, GPIO.HIGH)
+        sleep(.1)
+        GPIO.output(pin, GPIO.LOW)
+        sleep(.1)
+
+
+# blink(first_pin)
+# blink(second_pin)
+# blink(third_pin)
+
+
+first_thread = Thread(target=blink, args=[first_pin])
+second_thread = Thread(target=blink2, args=[second_pin])
+third_thread = Thread(target=blink, args=[third_pin])
+
+first_thread.start()
+second_thread.start()
+third_thread.start()
+
+first_thread.join()
+second_thread.join()
+third_thread.join()
+
+
+GPIO.cleanup()
